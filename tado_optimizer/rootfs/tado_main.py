19ca5b059d48533d85d2ea01105bd89d7fb82054
charlie-chester <m.chester@me.com>
2024-10-06 16:35:16 +0100

    Update logging configuration
    Reordered the logging initialization to include a configurable log level. Adjusted the position of WeatherAPI instantiation and bumped the version in the configuration file.

--- tado_optimizer/rootfs/tado_main.py
+++ tado_optimizer/rootfs/tado_main.py
@@ -5,25 +5,26 @@ import time
 from weather_api import WeatherAPI
 
 time.sleep(10)
-weather = WeatherAPI()
 
-logging.basicConfig(
-    level=logging.DEBUG, format="%(asctime)s : %(levelname)s : %(filename)s - %(lineno)d : %(message)s",datefmt="%Y-%m-%d %H:%M:%S")
-logging.info(msg="Tado Optimizer starting")
-
-# Access options file and obtain variables
 options_file = "/data/options.json"
 with open(options_file, "r") as file:
     options = yaml.safe_load(file)
 
+log_level = options.get('log_level', 'INFO').upper()
+logging.basicConfig(level=getattr(logging, log_level),
+                    format="%(asctime)s : %(levelname)s : %(filename)s - %(lineno)d : %(message)s",
+                    datefmt="%Y-%m-%d %H:%M:%S")
+
+logging.info(msg="Tado Optimizer starting")
+
 LONGITUDE = options.get("longitude")
 LATITUDE = options.get("latitude")
 OPEN_WEATHER_API_KEY = options.get("open_weather_api")
-
 logging.info(msg=f"The latitude is {LATITUDE}")
 logging.info(msg=f"The longitude is {LONGITUDE}")
 logging.info(msg=f"The Open Weather API Key is {OPEN_WEATHER_API_KEY}")
 
+weather = WeatherAPI()
 
 def main():
     weather.get_weather_data()

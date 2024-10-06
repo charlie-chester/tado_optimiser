77199e2b0ffa07adec26705c50fd3d1216279547
charlie-chester <m.chester@me.com>
2024-10-06 15:24:28 +0100

    Add WeatherAPI integration for weather data retrieval
    This commit introduces a new module, `weather_api.py`, which includes the `WeatherAPI` class for fetching weather data using OpenWeather API. Refactored `tado_main.py` to utilize this new class for improved code organization and readability. Updated `config.yaml` to reflect the new version and description changes.

--- tado_optimizer/rootfs/tado_main.py
+++ tado_optimizer/rootfs/tado_main.py
@@ -1,25 +1,24 @@
 import logging
 import yaml
+from weather_api import WeatherAPI
 
-logging.basicConfig(level=logging.INFO, format="%(asctime)s : %(levelname)s : %(filename)s line %(lineno)d : %(message)s",
-                    datefmt="%Y-%m-%d %H:%M:%S")
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s:%(levelname)s:%(filename)s line %(lineno)d: %(message)s",datefmt="%Y-%m-%d %H:%M:%S")
 logging.info(msg="Tado Optimizer starting")
 
-# Path to the options.yaml file
+# Access options file and obtain variables
 options_file = "/data/options.json"
-
-# Read the YAML configuration
 with open(options_file, "r") as file:
     options = yaml.safe_load(file)
 
-# Access the specific options
 LONGITUDE = options.get("longitude")
 LATITUDE = options.get("latitude")
 OPEN_WEATHER_API_KEY = options.get("open_weather_api")
 
+weather = WeatherAPI()
 
+logging.info(msg=f"The latitude is {LATITUDE}")
+logging.info(msg=f"The longitude is {LONGITUDE}")
+logging.info(msg=f"The Open Weather API Key is {OPEN_WEATHER_API_KEY}")
 
-logging.info(f"The latitude is {LATITUDE}")
-logging.info(f"The longitude is {LONGITUDE}")
-logging.info(f"The Open Weather API Key is {OPEN_WEATHER_API_KEY}")
-
+weather.get_weather_data()
---
+++ tado_optimizer/rootfs/weather_api.py
@@ -0,0 +1,27 @@
+import logging
+import yaml
+import requests
+
+BASE_URL = "https://api.openweathermap.org/data/3.0/onecall?"
+
+# Access options file and obtain variables
+options_file = "/data/options.json"
+with open(options_file, "r") as file:
+    options = yaml.safe_load(file)
+
+class WeatherAPI:
+    def __init__(self):
+        self.latitude = options.get("latitude")
+        self.longitude = options.get("longitude")
+        self.api_key = options.get("open_weather_api")
+
+    def get_weather_data(self):
+        fullUrl = f"{BASE_URL}lat={self.latitude}&lon={self.longitude}&appid={self.api_key}&units=metric"
+        data = requests.get(fullUrl)
+        status = data.status_code
+        logging.info(msg="Getting weather data")
+        logging.info(msg=f"Get weather data fullUrl: {fullUrl}")
+        logging.info(msg=f"Weather API status code: {status}")
+        return data.json()
+
+

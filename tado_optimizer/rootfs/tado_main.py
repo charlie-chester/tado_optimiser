d0ec6f831dd4c6d561d93e017f451ea72a3494b6
charlie-chester <133705626+charlie-chester@users.noreply.github.com>
2024-10-06 11:09:59 +0100

    Update config and main script for environment variables
    Increment version to 1.0.2 and add new configuration options and schema for longitude, latitude, and Open Weather API key. Adjust the main script to log these new environment variables and increase the sleep duration.

--- tado_optimizer/rootfs/tado_main.py
+++ tado_optimizer/rootfs/tado_main.py
@@ -1,13 +1,21 @@
 import logging
+import os
 import time
 import datetime
+
+LONGITUDE = os.getenv("HASSIO_LONGITUDE")
+LATITUDE = os.getenv("HASSIO_LATITUDE")
+OPEN_WEATHER_API_KEY = os.getenv("OPEN_WEATHER_API_KEY")
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)s : %(levelname)s : %(filename)s line %(lineno)d : %(message)s",
                     datefmt="%Y-%m-%d %H:%M:%S")
 
 logging.info(msg="Tado Optimizer starting")
 
-for i in range(10):
-    logging.info(msg=f"The time is {datetime.datetime.now()}")
-    time.sleep(10)
+logging.info(f"The longitude is {LONGITUDE}")
+logging.info(f"The latitude is {LATITUDE}")
+logging.info(f"The Open Weather API Key is {OPEN_WEATHER_API_KEY}")
+
+time.sleep(60 *60)
+
 

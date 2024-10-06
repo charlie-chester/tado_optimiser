41bee75e56464dbbc4e5b7ce86484d6e3dd17f18
charlie-chester <133705626+charlie-chester@users.noreply.github.com>
2024-10-06 12:08:57 +0100

    Update fetching configurations source to options.yaml file
    Changed the source of configuration parameters (longitude, latitude, and Open Weather API key) to options.yaml for improved flexibility. Also updated the version number in the config file to 1.0.9.

--- tado_optimizer/rootfs/tado_main.py
+++ tado_optimizer/rootfs/tado_main.py
@@ -1,14 +1,25 @@
 import logging
 import os
-
-LONGITUDE = os.getenv("HASSIO_LONGITUDE")
-LATITUDE = os.getenv("HASSIO_LATITUDE")
-OPEN_WEATHER_API_KEY = os.getenv("HASSIO_OPEN_WEATHER_API_KEY")
+import yaml
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)s : %(levelname)s : %(filename)s line %(lineno)d : %(message)s",
                     datefmt="%Y-%m-%d %H:%M:%S")
-
 logging.info(msg="Tado Optimizer starting")
+
+# Path to the options.yaml file
+options_file = "/data/options.yaml"
+
+# Read the YAML configuration
+with open(options_file, "r") as file:
+    options = yaml.safe_load(file)
+
+# Access the specific options
+LONGITUDE = options.get("longitude")
+LATITUDE = options.get("latitude")
+OPEN_WEATHER_API_KEY = options.get("open_weather_api")
+
+
+
 
 logging.info(f"The longitude is {LONGITUDE}")
 logging.info(f"The latitude is {LATITUDE}")

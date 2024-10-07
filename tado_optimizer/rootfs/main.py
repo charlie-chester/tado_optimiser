d2e263348bfc18b19e845f6b29313be0b71cd578
charlie-chester <133705626+charlie-chester@users.noreply.github.com>
2024-10-07 15:39:36 +0100

    Update config handling
    Revised configuration handling by changing the sleep duration, adding privileged API access, and utilizing JSON for the supervisor token. This ensures more efficient startup and better security with clear delineation of option sources.

--- tado_optimizer/rootfs/main.py
+++ tado_optimizer/rootfs/main.py
@@ -1,3 +1,4 @@
+import json
 import logging
 import time
 from datetime import datetime
@@ -6,17 +7,22 @@ import schedule
 from weather_api import WeatherAPI
 from hass import HomeAssistantAPI
 
-time.sleep(60)
+time.sleep(10)
 
-options_file = "/data/options.json"
-with open(options_file, "r") as file:
-    options = yaml.safe_load(file)
+options_addon = "/data/options.json"
+with open(options_addon, "r") as file:
+    addon_options = yaml.safe_load(file)
 
-LOG_LEVEL = options.get("log_level", "INFO")
-LATITUDE = options.get("latitude")
-LONGITUDE = options.get("longitude")
-OPEN_WEATHER_API = options.get("open_weather_api")
-TOKEN = options.get("token")
+LOG_LEVEL = addon_options.get("log_level", "INFO")
+LATITUDE = addon_options.get("latitude")
+LONGITUDE = addon_options.get("longitude")
+OPEN_WEATHER_API = addon_options.get("open_weather_api")
+
+options_supervisor = "/data/auth.json"
+with (open(options_supervisor, "r") as file):
+    supervisor_options = json.load(file)
+
+TOKEN = supervisor_options.get("access_token")
 
 logging.basicConfig(level=getattr(logging, LOG_LEVEL),
                     format="%(asctime)s %(levelname)s %(filename)s line %(lineno)d: %(message)s",
@@ -27,6 +33,7 @@ logging.info(msg="Tado Optimizer starting")
 logging.info(msg=f"The latitude is {LATITUDE}")
 logging.info(msg=f"The longitude is {LONGITUDE}")
 logging.info(msg=f"The Open Weather API Key is {OPEN_WEATHER_API}")
+logging.info(msg=f"The Home Assistant Token is {TOKEN}")
 
 weather = WeatherAPI(open_weather_api_key=OPEN_WEATHER_API, latitude=LATITUDE, longitude=LONGITUDE)
 hass = HomeAssistantAPI(token=TOKEN)

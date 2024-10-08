45e4314cb79db807cfe37302791f9ea48eab1810
charlie-chester <133705626+charlie-chester@users.noreply.github.com>
2024-10-08 08:52:13 +0100

    Increase initial delay and update version
    Extended the initial delay from 3 to 30 seconds to handle startup timing issues. Bumped the version from 1.0.28 to 1.0.29 accordingly.

--- tado_optimiser/rootfs/main.py
+++ tado_optimiser/rootfs/main.py
@@ -6,7 +6,7 @@ import schedule
 from weather_api import WeatherAPI
 
 
-time.sleep(3)
+time.sleep(30)
 options_file = "/data/options.json"
 with open(options_file, "r") as file:
     options = yaml.safe_load(file)
